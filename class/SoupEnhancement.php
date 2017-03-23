11f02a3117600a6628749f8a5d2684503d74d8ab
SM <s.marx@spa-gmbh.de>
2017-03-23 10:43:09 +0100

    first code, yeah

--- class/SoupEnhancement.php
+++ class/SoupEnhancement.php
@@ -17,3 +17,50 @@ class SoupEnhancement
         }
     }
 }
+
+// JS init example, not tested
+//
+//function bla()
+//{
+//    alert("bla");
+//}
+//
+//var modules = { "DOMContentLoaded": ["bla"]};
+//
+//document.addEventListener("DOMContentLoaded",function()
+//{
+//    alert("loaded");
+//    for(var i = 0; i< modules.DOMContentLoaded; i++)
+//  {
+//      windows[modules.DOMContentLoaded[i]]();
+//  }
+//});
+
+
+// inject files with JS
+//
+//function includeCSSfile(href) {
+//    var head_node = document.getElementsByTagName('head')[0];
+//    var link_tag = document.createElement('link');
+//    link_tag.setAttribute('rel', 'stylesheet');
+//    link_tag.setAttribute('type', 'text/css');
+//    link_tag.setAttribute('href', href);
+//    head_node.appendChild(link_tag);
+//}
+//
+//function includeCSSfile(href) {
+//    var head_node = document.getElementsByTagName('head')[0];
+//    var link_tag = document.createElement('link');
+//    link_tag.setAttribute('rel', 'stylesheet');
+//    link_tag.setAttribute('type', 'text/css');
+//    link_tag.setAttribute('href', href);
+//    head_node.appendChild(link_tag);
+//}
+//
+//OR
+//
+//var link = document.createElement("link");
+//link.href = "http://example.com/mystyle.css";
+//link.type = "text/css";
+//link.rel = "stylesheet";
+//document.getElementsByTagName("head")[0].appendChild(link);
